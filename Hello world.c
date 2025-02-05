fbc747269608542c3ef3f1d8a90666eb22acbf06
Mayank457 <mayankshekhar7324@gmail.com>
2025-02-05 19:59:05 +0530

    Add files via upload

---
+++ Hello world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+
+    //printf("Hello World\n");
+    printf("Mayank\n");
+    printf("krishna\n");
+    printf("JAi shree ram\n");
+    printf("Radhe Radhe\n");
+    printf("Premanand Maharaj ki jai\n");
+    printf("Jai shree krishna\n");
+    return 0;
+}
