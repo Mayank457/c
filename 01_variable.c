f2de90a998f303b117a33e949d9f1174c018f12b
Mayank457 <mayankshekhar7324@gmail.com>
2025-02-05 19:59:46 +0530

    Add files via upload

---
+++ 01_variable.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int main(){
+int a; // variable declaration 
+a = 6; // Variable initialization
+printf("%d\n", a);
+printf("the output of this program is %d, a\n");
+int b; // variable decleration
+b = 7; // variable intialization
+printf("%d\n", b);
+printf("The output of this programme is %d\n", b);
+printf("result is %d\n", b);
+printf("result is %d\n", b);
+printf("result is %d\n", b);
+printf("result is %d\n", b);
+return 0;
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
