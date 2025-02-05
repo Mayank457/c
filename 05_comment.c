889d726f7df98c60a5c5937cc62a59a805dcf864
Mayank457 <mayankshekhar7324@gmail.com>
2025-02-05 20:00:14 +0530

    Add files via upload

---
+++ 05_comment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+program : 05_comment.c
+author : Harry
+Date   : 15th dec*/
+int main() {
+    // I am writting this just for fun. this line doesnot do anything
+    printf("Hello, World!\n");
+    // this is return to 0
+    return 0;
+}
